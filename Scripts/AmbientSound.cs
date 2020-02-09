62f61a2b9942227819d332767c5be37349f560b3
Albarnie <albertedgelltaylor@gmail.com>
2020-02-09 13:19:37 +1100

    Initial commit

---
+++ Scripts/AmbientSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientSound : MonoBehaviour
+{
+    AudioSource source;
+
+    [SerializeField]
+    float minPitch = 0.9f,
+        maxPitch = 1.1f;
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+
+        source.pitch *= Random.Range(minPitch, maxPitch);
+
+        source.time = Random.Range(0, source.clip.length);
+    }
+}
---
+++ Scripts/AudioZone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class AudioZone
+{
+    public string name = "AudioZone";
+
+    public float volume = 1f;
+
+    public AudioMixerSnapshot snapshot;
+    public AudioClip ambientSound;
+
+    public float blendTime = 5f;
+}
